3e3b1baa4c47fef8a2e4b2fa4eb5880cce4353c7
Mike Meyer <github@meyer.fm>
2014-01-20 05:09:26 -0800

    mo errors, rudimentary timing

--- parse.rb
+++ parse.rb
@@ -6,6 +6,9 @@ require "twitter-text"
 
 require "addressable/uri"
 require "httpclient"
+require "openssl"
+
+# OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 
 IMG_CACHE_PATH = File.expand_path "./image_cache"
 # `mkdir -p #{Shellwords.escape IMG_CACHE_PATH}"`
@@ -24,15 +27,27 @@ expanded_urls = {}
 
 @client = HTTPClient.new
 
+# ???
+@client.connect_timeout = 2
+@client.keep_alive_timeout = 2
+@client.ssl_config.verify_mode = OpenSSL::SSL::VERIFY_NONE
+
+def time
+  start = Time.now
+yield
+  mm, ss = (Time.now - start).divmod(60)
+  hh, mm = mm.divmod(60)
+  puts "Time elapsed: %d hours, %d minutes and %d seconds" % [hh, mm, ss]
+end
+
 def expand_url(url, depth=0)
   depth += 1
   begin
-    puts "==============" if depth == 1
     uri = Addressable::URI.parse(url).normalize
-    puts "[#{depth}] Expanding #{uri}"
+    puts "[#{depth}] Expanding #{uri.to_s.length > 83 ? "#{uri.to_s[0...80]}..." : uri.to_s}"
 
+    #Y U no resolve http://xn--df-oiy.ws/ere!!
     res = @client.get(url)
-    puts "#{res}"
 
     if res.status_code == 200
       return ["200", url]
@@ -40,7 +55,7 @@ def expand_url(url, depth=0)
       unless url == res.header["Location"][0]
         location = res.header["Location"][0]
         unless location =~ /^https?:\/\//
-          puts "--- 'Location' did not include path"
+          puts " |  'Location' did not include path"
           location = "#{uri.scheme}://#{uri.host}/#{location.gsub(/^\//,"")}"
         end
         return expand_url(location, depth)
@@ -54,12 +69,20 @@ def expand_url(url, depth=0)
     raise
   rescue SocketError
     # DNS resolution error
-    puts "  - DNS failed to resolve"
+    puts "--- DNS failed to resolve"
+    return ["---", url]
+  rescue OpenSSL::SSL::SSLError
+    puts "--- SSL Error!"
+    return ["---", url]
+  rescue HTTPClient::ConnectTimeoutError
+    puts "--- Connection timed out"
     return ["---", url]
   end
 end
 
 Dir.chdir("./tweets/data/js/tweets")
+
+puts "\nTweets by month:", "================"
 Dir.glob("*.js") do |p|
   # get JSON
   tweets_by_month = JSON.parse(IO.read(p).lines.to_a[1..-1].join)
@@ -77,8 +100,7 @@ Dir.glob("*.js") do |p|
     end
     shared_media.concat v["entities"]["media"]
   end
-
-  # puts "#{p}: #{tweets_by_month.length} tweets"
+  puts "#{p}: #{tweets_by_month.length} tweets"
   tweets.concat tweets_by_month
 end
 
@@ -89,27 +111,31 @@ puts "#{shared_hashtags.length} hashtags in total"
 puts "#{shared_urls.length} URLs in total"
 
 Dir.chdir IMG_CACHE_PATH
-puts "","Let’s get this party started!",""
+puts "","Let’s get this party started!"
 
-# Build array of shared URLs
-shared_urls.each do |s|
-  url = "#{s}"
-  url = "http://#{url}" unless url =~ /^https?:\/\//
+time do
+  # Build array of shared URLs
+  shared_urls.each_with_index do |s, idx|
+    url = "#{s}"
+    url = "http://#{url}" unless url =~ /^https?:\/\//
 
-  # Filter out invalid URLs
-  status_code, expanded_url = expand_url(url)
+    puts "\n=== #{idx+1} of #{shared_urls.length} ==========="
 
-  # Using addressable to deal with IDNs
-  u = Addressable::URI.parse(expanded_url).normalize
-  hostname = u.host.to_s.downcase
+    # Filter out invalid URLs
+    status_code, expanded_url = expand_url(url)
 
-  # Pretty dumb regex but whatever idgaf
-  hostname = $~[:url] if hostname =~ /(?<url>[\w\-]+(?:\.\w{2,3}){1,2})$/
+    # Using addressable to deal with IDNs
+    u = Addressable::URI.parse(expanded_url).normalize
+    hostname = u.host.to_s.downcase
 
-  urls_by_hostname[hostname] ||= []
-  urls_by_hostname[hostname].push expanded_url
+    # Pretty dumb regex but whatever idgaf
+    hostname = $~[:url] if hostname =~ /(?<url>[\w\-]+(?:\.\w{2,3}){1,2})$/
 
-  puts "#{status_code} #{expanded_url}"
+    urls_by_hostname[hostname] ||= []
+    urls_by_hostname[hostname].push expanded_url
+
+    puts "#{status_code} #{expanded_url.to_s.length > 83 ? "#{expanded_url.to_s[0...80]}..." : expanded_url.to_s}"
+  end
 end
 
 puts "\nShared URLs"
